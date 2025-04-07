5f004c7d94c3c67ddc67d2857fe1ebb7f26bfb01
Michael Tabor <michael.tabor@sigit.us>
2025-04-07 13:56:59 -0600

    wome work

--- index.js
+++ index.js
@@ -45,3 +45,31 @@ const bookStore = {
 
 // Write your code here!
 
+const bookStoreTitle = document.getElementById('header');
+bookStoreTitle.innerHTML = bookStore.name;
+
+const bookList = document.getElementById('book-list');
+
+
+bookStore.books.forEach((book) => {
+
+  const bookContainer = document.createElement('li');
+  const bookTitle = document.createElement('h3');
+  const bookAuthor = document.createElement('p');
+  const bookImage = document.createElement('img');
+
+  bookContainer.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookImage.src = book.imageUrl;
+  bookImage.alt = book.title;
+
+  bookContainer.append(bookTitle);
+  bookContainer.append(bookAuthor);
+  bookContainer.append(bookImage);
+
+  bookList.append(bookContainer);
+
+
+})
+
+
